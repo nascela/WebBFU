d9a83807c4606ca89e397fd6eaebc3ba593971a9
nascela <serjono5522@gmail.com>
2022-02-11 15:13:55 +0200

    2.digits

--- code/index.php
+++ code/index.php
@@ -5,4 +5,15 @@ $order = &$very_bad_unclear_name;
 
 $order .=" and a bear";
 
- echo"\nYour order is:$very_bad_unclear_name.";
+ echo"\nYour order is:$very_bad_unclear_name."."<br>";
+//числа
+$Number = 42;
+echo $Number."<br>";
+$MyNumber = 13;
+echo $MyNumber."<br>";
+$enotherNumber = 6.66;
+echo $enotherNumber."<br>";
+echo ($Number - $MyNumber - 17)."<br>";
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo ($lastMonth-$thisMonth)."<br>";
