e3f0be0d0d2eb0fcfe95ff94c24574885271a16f
nascela <serjono5522@gmail.com>
2022-02-11 15:24:34 +0200

    3.Multiplication and Division

--- code/index.php
+++ code/index.php
@@ -17,3 +17,9 @@ echo ($Number - $MyNumber - 17)."<br>";
 $lastMonth = 1187.23;
 $thisMonth = 1089.98;
 echo ($lastMonth-$thisMonth)."<br>";
+//11(3) Умножение и деление
+$numLanguages = 4;
+$months = 11;
+$days = 16;
+$daysPerLanguage = ($days * $months) / $numLanguages;
+echo "$daysPerLanguage";
