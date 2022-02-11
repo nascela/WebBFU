248426ce21c9850ee1ea9a8e1dae92e550efeda5
nascela <serjono5522@gmail.com>
2022-02-11 14:38:30 +0200

    update index.php (punkt 1)

--- code/index.php
+++ code/index.php
@@ -1,6 +1,8 @@
 <?php
 /* Imagine a lot of code here */
 $very_bad_unclear_name="15 chicken wings";
-// Write your code here:
-// Don't change the line below
+$order = &$very_bad_unclear_name;
+
+$order .=" and a bear";
+
  echo"\nYour order is:$very_bad_unclear_name.";
