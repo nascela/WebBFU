649b6d131f35137258cc813976cb46f9d6ab3fd3
nascela <serjono5522@gmail.com>
2022-02-25 18:31:52 +0200

    2.a

--- code/lab3.php
+++ code/lab3.php
@@ -1,8 +1,8 @@
-
-<!-- 1.Регулярные выражения. -->
-
-<!-- пункт a -->
 <?php
+session_start();
+// <!-- 1.Регулярные выражения. -->
+
+// <!-- пункт a -->
 echo '1.Регулярные выражения<br>';
 $regexp = '/a.{2}b/';
 $str='ahb acb aeb aeeb adcb axeb';
@@ -12,7 +12,7 @@ print_r($matches);
 echo "<br>";
 echo "<br>";
 
-// <!-- пункт b -->
+//  пункт b 
 
 $regexp = '/\d+/';
 $str = 'a1b2c3';
@@ -30,4 +30,43 @@ $result = preg_replace_callback(
 echo $result . '<br>';
 echo "<br>";
 
+// 2.Форма сессии и куки
+// пункт a
 
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>reg exp and file work</title>
+</head>
+<body>
+<form method="POST">
+    <label>
+<textarea name="textToParse"
+          placeholder="INput some text"
+          rows="5" cols="40"></textarea>
+    </label>
+    <input type="submit" name="getWordsAndSyms" value="text treatment ">
+</form>
+</body>
+</html>
+
+<?php
+if ($_POST['getWordsAndSyms']) {
+    if ($_POST['textToParse']) {
+        $_SESSION['textInfo'] = str_word_count($_POST['textToParse'],
+                0) . ' Words<br>';
+        
+        $_SESSION['textInfo'] .= strlen($_POST['textToParse']) . ' Symbols<br>';
+        
+    } else {
+        $_SESSION['textInfo'] = 'No text';
+    }
+}
+echo $_SESSION['textInfo'];
