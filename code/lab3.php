cc36ccc277b13e2e56e6b702a281ff438c891952
nascela <serjono5522@gmail.com>
2022-02-25 18:02:23 +0200

    1.b)reg exp

--- code/lab3.php
+++ code/lab3.php
@@ -1,6 +1,6 @@
-<!-- /*
-  1.Регулярные выражения.
-*/ -->
+
+<!-- 1.Регулярные выражения. -->
+
 <!-- пункт a -->
 <?php
 echo '1.Регулярные выражения<br>';
@@ -9,3 +9,25 @@ $str='ahb acb aeb aeeb adcb axeb';
 $matches = [];
 $count = preg_match_all($regexp, $str, $matches);
 print_r($matches);
+echo "<br>";
+echo "<br>";
+
+// <!-- пункт b -->
+
+$regexp = '/\d+/';
+$str = 'a1b2c3';
+echo $str . ' -> ';
+$result = preg_replace_callback(
+    $regexp,
+    function ($matches) {
+        foreach ($matches as &$item) {
+            $item = $item ** 3;
+        }
+        return $matches[0];
+    },
+    $str
+);
+echo $result . '<br>';
+echo "<br>";
+
+
