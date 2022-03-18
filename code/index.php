b30964031e60044333367daf52973e18b23a348e
nascela <serjono5522@gmail.com>
2022-03-18 22:26:55 +0200

    Lab 4

--- code/index.php
+++ code/index.php
@@ -3,11 +3,15 @@
 
 echo<<<HTML
     <div>
+    <a href="/lab4.php">lab4</a>
+    </div>
+    <div>
     <a href="/lab3.php">lab3</a>
     </div>
     <div>
     <a href="/lab2.php">lab2</a>
     </div>
+    
 HTML;
 
 
